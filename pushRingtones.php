13b0c35b826a7031bd471e791dd6c812e712db0a
Felipe Lecot <47688222+FelipeLec@users.noreply.github.com>
2021-12-17 16:15:26 -0300

    Upload ringtone working

--- pushRingtones.php
+++ pushRingtones.php
@@ -2,8 +2,7 @@
     header('Access-Control-Allow-Origin: *');
 	include 'Login.php';
 
-    $file = $_POST['file'];
-    $rawDataJSON = $_POST['data'];
+    $rawDataJSON = strip_tags(trim($_POST['data']));
 
     if (isset($_FILES['file'])) {
         $file_extension = end(explode(".", $_FILES["file"]["name"]));
@@ -14,12 +13,13 @@
 
         $result = mysqli_query($connect,$query);
 
-        mysqli_close($connect);
-
         if ($_FILES["file"]["size"] < 5000000 && $file_extension == "mp3") {
+            $fileId = mysqli_insert_id($connect);
             $sourcePath = $_FILES['file']['tmp_name'];
-            $targetPath = "media/" . mysqli_insert_id($connect); . ".mp3";
+            $targetPath = "media/" . $fileId . ".mp3";
             move_uploaded_file($sourcePath, $targetPath);
         }
+
+        mysqli_close($connect);
     }
 ?>
