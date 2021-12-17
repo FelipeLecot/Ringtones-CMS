da3a951396fc830cedd45ede9f2788d07235ba00
Felipe Lecot <47688222+FelipeLec@users.noreply.github.com>
2021-12-17 18:22:08 -0300

    Exported final version

--- addValueToRingtone.php
+++ addValueToRingtone.php
@@ -3,6 +3,11 @@
 	include 'Login.php';
 
     $type = $_POST['type'];
+    $id = $_POST['id'];
 
-    
+    $connect = mysqli_connect($host, $db_username, $db_password, $db_name);
+
+    $query = "UPDATE ringtones SET $type = $type + 1 WHERE id = '$id'";
+
+    $result = mysqli_query($connect,$query);
 ?>
