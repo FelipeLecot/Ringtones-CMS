d59ad41da7c829f183b5ec0cd290241979081968
Felipe Lecot <47688222+FelipeLec@users.noreply.github.com>
2022-01-02 07:33:13 -0300

    Update getRingtones.php

--- getRingtones.php
+++ getRingtones.php
@@ -2,7 +2,7 @@
     header('Access-Control-Allow-Origin: *');
 	include 'Login.php';
 
-    $limit = $_GET['limit'];
+    $id = $_GET['id'];
     $orderBy = $_GET['order'];
     $cat = $_GET['cat'];
     $last = $_GET['latest'];
@@ -17,6 +17,12 @@
         }
         else {
             return 'ORDER BY downloads DESC ';
+        }
+    }
+
+    function byId($id) {
+        if ($id != '' && $id != null) {
+            return "AND id = '$id' "
         }
     }
 
@@ -80,7 +86,7 @@
 
     $connect = mysqli_connect($host, $db_username, $db_password, $db_name);
 
-    $query = "SELECT id, likes, ringtoneData, downloads FROM ringtones WHERE id > $last " . getCategory($cat) . getSearchParams($search) . orderBy($orderBy) . "LIMIT $limit";
+    $query = "SELECT id, likes, ringtoneData, downloads FROM ringtones WHERE id > $last " . getCategory($cat) . getSearchParams($search). byId($id) . orderBy($orderBy) . "LIMIT $limit";
     
     $result = mysqli_query($connect,$query);
 
